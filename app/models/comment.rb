9f2eb2e86f664a4d5e140cb8ca8672ab7dcf056f
Adam Cohn <adamcohn88@gmail.com>
2018-05-24 11:59:41 -0400

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,10 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
+  accepts_nested_attributes_for :user, reject_if: :user_rejectable?
+  private
 
+ def user_rejectable?(att)
+  att['username'].blank? && new_record?
+ end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Post.create(title: "a post", content: "some content")
+Category.create(name: "a category")
+User.create(username: "Bob", email: "bob@bob.com")
+Comment.create(content: "A comment.")
